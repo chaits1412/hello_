3fb3644d5c0c8505ca14ff520c96bb20b969cdd7
chaits1412 <160209690+chaits1412@users.noreply.github.com>
2024-02-20 12:07:17 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,23 @@
+# Python program to print first 10
+# prime numbers
+
+# A function name prime is defined
+# using def
+def python_def_prime(n):
+	x = 2
+	count = 0
+	while count < n:
+		for d in range(2, x, 1):
+			if x % d == 0:
+				x += 1
+		else:
+			print(x)
+			x += 1
+			count += 1
+
+# Driver Code
+n = 10
+
+# print statement
+print("First 10 prime numbers are: ")
+python_def_prime(n)
